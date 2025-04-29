a25584aaa6e1da1b97909085b1364112949b7963
UkejeO <uokoron1@UkPC>
2025-04-29 13:12:33 -0400

    please work

--- p4/flask_app/models.py
+++ p4/flask_app/models.py
@@ -1,60 +1,31 @@
 from flask_login import UserMixin
 from datetime import datetime
-# from . import db, login_manager
-from flask_login import UserMixin
-from datetime import datetime
-from .extensions import db, login_manager
-from mongoengine import StringField, ReferenceField, EmailField, ImageField
-# from mongoengine import FileField as MongoFileField
+from . import db, login_manager
+from mongoengine import StringField, ReferenceField, EmailField, ImageField, ListField
 
-# TODO: implement
 @login_manager.user_loader
 def load_user(user_id):
     return User.objects(username=user_id).first()
 
-# TODO: implement fields
 class User(db.Document, UserMixin):
     username = StringField(required=True, unique=True, min_length=1, max_length=40)
     email = EmailField(required=True, unique=True)
-    password =  StringField(required=True)
+    password = StringField(required=True)
     profile_pic = ImageField()
 
-    # Returns unique string identifying our object
     def get_id(self):
         return self.username
 
-
-# TODO: implement fields
-# instead of reviews will change this to user posts
-
-# Rename Review to Post_comments for posts 
-# class Review(db.Document):
-#     commenter = ReferenceField(User, required=True)
-#     content = StringField(required=True, min_length=5, max_length=500)
-#     date = StringField(required=True, default=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-#     imdb_id = StringField(required=True, min_length=9, max_length=9)
-#     movie_title = StringField(required=True, min_length=1, max_length=100)
-
-class PostComment(db.Document):
-    commenter = ReferenceField("User", required=True)  # who made the comment
-    post = ReferenceField("Post", required=True)        # what Post the comment belongs to
-    content = StringField(required=True, min_length=1, max_length=500)
-    date = StringField(default=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-
 class Post(db.Document):
-    author = ReferenceField("User", required=True)
+    author = ReferenceField(User, required=True)
     content = StringField(required=True, min_length=5, max_length=500)
-    category = StringField(required=True, choices=["sports", "news", "entertainment"])  # New field
-    image = ImageField()  # Replace movie poster with user-uploaded image
-<<<<<<< HEAD
-    date = StringField(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    category = StringField(required=True, choices=["sports", "news", "entertainment"])
+    image = ImageField()
+    date = StringField(default=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    likes = ListField(ReferenceField(User), default=[])
 
 class Comment(db.Document):
     post = ReferenceField(Post, required=True)
     commenter = ReferenceField(User, required=True)
     content = StringField(required=True, min_length=1, max_length=500)
     date = StringField(default=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-=======
-    date = StringField(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))    
->>>>>>> a0997d9a870d24891f1182c64ee02a777f4e41e9
--- p4/flask_app/posts/routes.py
+++ p4/flask_app/posts/routes.py
@@ -1,5 +1,4 @@
 import base64, io
-<<<<<<< HEAD
 from flask import Blueprint, render_template, url_for, redirect, request, flash, current_app
 from flask_login import current_user, login_required
 from mongoengine.queryset.visitor import Q
@@ -8,22 +7,8 @@ from .. import post_client
 from ..forms import PostForm, CommentForm, SearchForm
 from ..models import User, Post, Comment
 from ..utils import current_time
-=======
-from flask import Blueprint, render_template, url_for, redirect, request, flash
-from flask_login import current_user
-from ..forms import PostForm, SearchForm, CommentForm  # Changed form
-from ..models import User, Post  # Changed model
-from ..utils import current_time
-from ..aws import upload_file  # ✅
-from flask_login import login_required
-from .. import PostClient
-from ..models import Post, PostComment
-from flask import send_file
-import io
 
->>>>>>> a0997d9a870d24891f1182c64ee02a777f4e41e9
-
-posts = Blueprint("posts", __name__) 
+posts = Blueprint("posts", __name__)  # Make sure this is "posts" not "movies"
 
 def get_b64_img(username):
     user = User.objects(username=username).first()
@@ -47,7 +32,6 @@ def index():
     
     return render_template("index.html", form=form, posts=recent_posts)
 
-<<<<<<< HEAD
 @posts.route("/search-results/<query>", methods=["GET"])
 def query_results(query):
     try:
@@ -55,25 +39,12 @@ def query_results(query):
         return render_template("query.html", results=results, query=query)
     except ValueError as e:
         return render_template("query.html", error_msg=str(e), query=query)
-=======
-@posts.route("/search/<category>")
-def search_by_category(category):
-    valid_categories = ["sports", "news", "entertainment"]
-    if category.lower() not in valid_categories:
-        flash("Invalid category.", "danger")
-        return redirect(url_for("posts.index"))
-
-    posts = Post.objects(category=category.lower())
-    return render_template("search_results.html", posts=posts, category=category)
-
->>>>>>> a0997d9a870d24891f1182c64ee02a777f4e41e9
 
 @posts.route("/create-post", methods=["GET", "POST"])
 @login_required
 def create_post():
     form = PostForm()
     if form.validate_on_submit():
-<<<<<<< HEAD
         # Create a new post
         new_post = Post(
             author=current_user._get_current_object(),
@@ -89,25 +60,9 @@ def create_post():
         new_post.save()
         flash("Post created successfully!", "success")
         return redirect(url_for("posts.index"))
-=======
-            
-        new_post = Post(
-        author=current_user._get_current_object(),
-        content=form.content.data,
-        category=form.category.data,
-        )
-
-        if form.image.data:
-            new_post.image.put(form.image.data, content_type=form.image.data.content_type)
-
-        new_post.save()
-
-        return redirect(url_for("posts.post_detail", post_id=str(new_post.id)))
->>>>>>> a0997d9a870d24891f1182c64ee02a777f4e41e9
     
-    return render_template("userPost.html", form=form)
+    return render_template("create_post.html", form=form)
 
-<<<<<<< HEAD
 @posts.route("/post/<post_id>", methods=["GET", "POST"])
 def post_detail(post_id):
     try:
@@ -146,41 +101,6 @@ def post_detail(post_id):
 def category_posts(category):
     posts = post_client.get_posts_by_category(category)
     return render_template("category.html", posts=posts, category=category)
-=======
-@posts.route("/post/<post_id>", methods=["GET", "POST"])  # ✅ ALLOW POST HERE
-def post_detail(post_id):
-    post = Post.objects.get_or_404(id=post_id)
-    comments = PostComment.objects(post=post.id)
-    form = CommentForm()
-
-    if form.validate_on_submit() and current_user.is_authenticated:
-        comment = PostComment(
-            commenter=current_user._get_current_object(),
-            post=post,
-            content=form.content.data
-        )
-        comment.save()
-        flash("Comment added!", "success")
-        return redirect(url_for("posts.post_detail", post_id=post.id))
-
-    return render_template("post_detail.html", post=post, comments=comments, comment_form=form)
-
-
-
-@posts.route("/post/<post_id>/image")
-def serve_image(post_id):
-    post = Post.objects.get_or_404(id=post_id)
-
-    if post.image:
-        return send_file(
-            io.BytesIO(post.image.read()),
-            mimetype=post.image.content_type
-        )
-    else:
-        return "No image found", 404
-
-
->>>>>>> a0997d9a870d24891f1182c64ee02a777f4e41e9
 
 @posts.route("/user/<username>")
 def user_detail(username):
