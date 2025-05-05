01cf4589014fabcb2fe869a25453630c40122605
EverC0 <everrjc@gmail.com>
2025-05-05 19:49:58 -0400

    continue

--- p4/flask_app/config.py
+++ p4/flask_app/config.py
@@ -1,6 +1,11 @@
-# Stores all configuration values
-SECRET_KEY = b'\x9e\xb1\xe7\xe60z\x08\\A\xc1\xc6w\xb5\xa4\xf8\xac'
-MONGODB_HOST = 'mongodb+srv://Everco1:Obed2003@cluster0.pzodnix.mongodb.net/week4?retryWrites=true&w=majority&appName=Cluster0'
+import os
 
-GOOGLE_CLIENT_ID = "1081114959839-tm7ks5ljqf9k37b90if6if6bdv6mt55k.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+
+# print(f"SECRET_KEY: {SECRET_KEY}")
+# print(f"MONGODB_HOST: {MONGODB_HOST}")
+# print(f"GOOGLE_CLIENT_ID: {GOOGLE_CLIENT_ID}")
+# print(f"GOOGLE_CLIENT_SECRET: {GOOGLE_CLIENT_SECRET}")
--- p4/run.py
+++ p4/run.py
@@ -1,4 +1,9 @@
-from flask_app import create_app
+from dotenv import load_dotenv
 import os
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  
+
+load_dotenv()  # This must be before create_app()
+
+from flask_app import create_app
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 app = create_app()
